eaacff64b9d1a780daf69ea214a53e40f094ca46
Norman <normanius@gmail.com>
2021-12-29 14:39:26 +0100

    Add _utils.search_files() to find files matching a glob-expression

--- dicom_tools/_utils.py
+++ dicom_tools/_utils.py
@@ -40,6 +40,21 @@ def ensure_out_dir(out_dir: PathLike, raise_error: bool=False) -> bool:
     return out_dir.is_dir()
 
 
+def search_files(in_dir: PathLike,
+                 pattern: Optional[str]=None,
+                 n_files: Optional[int]=None) -> List[Path]:
+    if pattern is None:
+        pattern = "*.*"
+    in_dir = Path(in_dir)
+    files = sorted(list(in_dir.glob(pattern)))
+    if n_files is not None and n_files > 0:
+        files = files[:n_files]
+    if len(files) == 0:
+        _logger.error("No files found: %s", in_dir)
+        _logger.error("Pattern: %s", pattern)
+    return files
+
+
 def create_progress_bar(size: Optional[int]=None,
                         label: str="Processing...",
                         threaded: bool=False,
