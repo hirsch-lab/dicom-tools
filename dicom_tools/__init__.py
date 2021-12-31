13c0e5d04e285750983cc96454f7354dd6671a13
Norman <normanius@gmail.com>
2021-12-31 13:05:40 +0100

    Update requirements.txt, add pyyaml dependency

--- dicom_tools/__init__.py
+++ dicom_tools/__init__.py
@@ -4,4 +4,8 @@ __author__ = "Norman Juchler"
 from ._dicom_io import (copy_from_file,
                         copy_from_list,
                         copy_headers,
-                        print_info)
+                        print_info,
+                        #stack2dicom,
+                        #nifti2dicom,
+                        #dicom2nifti,
+                        )
