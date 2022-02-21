b0f15cb98b722ea397ce6873d828a8d0d81f5d9a
saro92 <sada.roth@gmail.com>
2022-02-21 12:47:18 +0100

    Update: Add compression and reorient parameter

--- dicom_tools/scripts/dicoms_to_nifti.py
+++ dicom_tools/scripts/dicoms_to_nifti.py
@@ -3,7 +3,6 @@ sys.path.append("/Users/sandroroth/Documents/Pycharm/dicom_tool/dicom-tools")
 
 import logging
 import argparse
-from pathlib import Path
 
 from dicom_tools._utils import setup_logging
 from dicom_tools._conversion import dicom_2_nifti
@@ -13,7 +12,10 @@ _logger = logging.getLogger(LOGGER_ID)
 
 def _run(args):
     setup_logging(verbosity=args.verbosity + 1)
-    dicom_2_nifti(in_dir=args.in_dir, out_dir=args.out_dir)
+    dicom_2_nifti(in_dir=args.in_dir,
+                  out_dir=args.out_dir,
+                  comp=args.compression,
+                  reor=args.reorient)
 
 
 def _parse_args():
@@ -43,12 +45,12 @@ def _parse_args():
     group.add_argument("-f", "--force", action="store_true",
                        help="Force writing of output files.")
 
-    # Nifti (how to change? to make it work?)
-    # group = parser.add_argument_gourp("Nifti")
-    # group.add_argument("-c", "--compression", default=True, type=bool,
-    #                    help=("Compresseion to nii.gz. Default: True"))
-    # group.add_argument("-r", "--reorient", default=True, type=bool,
-    #                    help=("Reorientation of dicom data to LAS orientation. Default: True"))
+    # Nifti
+    group = parser.add_argument_group("Nifti")
+    group.add_argument("-c", "--compression", default=True, type=bool,
+                       help=("Compression to nii.gz. Default: True"))
+    group.add_argument("-r", "--reorient", default=False, type=bool,
+                       help=("Reorientation of DICOM data to LAS orientation. Default: False"))
 
 
     group.set_defaults(func=_run)
