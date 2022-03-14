10af4b43a46010b07db1ff5ac813e5a08ff8621f
saro92 <sada.roth@gmail.com>
2022-03-14 17:46:18 +0100

    Update: Fixed ds.Pixelrepresentation, added image.modes to _read_image function and global pixel-depth flag

--- dicom_tools/_conversion.py
+++ dicom_tools/_conversion.py
@@ -1,15 +1,13 @@
 import logging
 import dicom2nifti
 import nibabel as nib
-import cv2 as cv
 import numpy as np
 import pydicom as dicom
+import pynetdicom
+import tempfile
+
 from PIL import Image
-from dicom2nifti import common
-
-
 from pathlib import Path
-
 from ._utils import (search_files, create_progress_bar, ensure_out_dir, resolve_multiframe)
 
 _LOGGER_ID = "dicom"
@@ -22,15 +20,37 @@ from typing import TypeVar, Optional, Any
 PathLike = TypeVar("PathLike", str, Path)
 
 
-def _read_image(path: PathLike) -> Optional[np.ndarray]:
+def _read_image(path: PathLike,
+                depth: Optional[str]=None) -> Optional[np.ndarray]:
     path = Path(path)
     if not path.is_file():
         _logger.error("File does not exist: %s", path)
         return None
-    # check for RGB! if there exception! len(img.shape) > 1
+
     img = Image.open(str(path))
+    # Turn multi-channel images into grayscale (only able to make 8bit images)
+    c_list = ["RGB", "RGBA", "CMYK", "YCbCr",
+              "LAB", "HSV", "RGBX", "RGBa",
+              "BGR;15", "BGR;16", "BGR;24",
+              "BGR;32"]
+    if img.mode in c_list:
+        _logger.info("Caution! Larger than 8-bit multi-channel images \n"
+                     "are converted to 8-bit grayscale images\n"
+                     "current image dtype: {}".format(img.mode))
+        img = img.convert('L')
     img.load()
-    img = np.asarray(img, dtype="uint8")
+
+    # Need to implement pixel-depth conversion?
+    # Numpy is able to read pixel-depth loaded by PIL and therefore
+    # switches to right dtype by default. However only up to 16Bit Images
+    # have been tested with PIL.
+
+    sup_depth = ["L","P", "I", "F", "LA", "PA",
+                 "La", "I;16", "I;16L", "I;16B",
+                 "I;16N"]
+    assert img.mode in sup_depth, ("The current pixel-depth is not recognised\n"
+                                  "by PIL")
+    img = np.asarray(img, dtype=depth)
     if img is None:
         _logger.error("Cannot read image: %s", path)
         return None
@@ -45,15 +65,17 @@ def _infer_sopclass_uid(storage_type: Optional[str]=None) -> Optional[str]:
         sopclass_uid = None
     elif storage_type == "CT":
         # sopclass_uid = "1.2.840.10008.5.1.4.1.1.2"
-        sopclass_uid = dicom._storage_sopclass_uids.CTImageStorage
+        # sopclass_uid = dicom._storage_sopclass_uids.CTImageStorage
+        sopclass_uid = pynetdicom.sop_class.CTImageStorage
     elif storage_type in ("MRI", "MR"):
         # sopclass_uid = "1.2.840.10008.5.1.4.1.1.4"
-        sopclass_uid = dicom._storage_sopclass_uids.MRImageStorage
+        # sopclass_uid = dicom._storage_sopclass_uids.MRImageStorage
+        sopclass_uid = pynetdicom.sop_class.MRImageStorage
     elif (storage_type in dicom.uid.UID_dictionary and
           dicom.uid.UID_dictionary[storage_type][1] == "SOP Class"):
         sopclass_uid = storage_type
-    elif hasattr(dicom._storage_sopclass_uids, storage_type):
-        sopclass_uid = getattr(dicom._storage_sopclass_uids, storage_type)
+    # elif hasattr(dicom._storage_sopclass_uids, storage_type):
+    #     sopclass_uid = getattr(dicom._storage_sopclass_uids, storage_type)
     else:
         msg = "This storage SOP class is not supported: %s"
         assert False, msg % storage_type
@@ -73,9 +95,13 @@ def _default_meta(storage_type: Optional[str]=None
 
 
 def _apply_attributes(data: dicom.Dataset,
+                      meta: dicom.dataset.FileMetaDataset,
                       attributes: Optional[dicom.Dataset]) -> None:
     for elem in attributes:
         data[elem.tag] = elem
+
+    for elem in attributes.file_meta:
+        meta[elem.tag] = elem
 
 
 def _ndarray2dicom(data: np.ndarray,
@@ -85,14 +111,19 @@ def _ndarray2dicom(data: np.ndarray,
     This method is inspired by inputs from here:
     https://stackoverflow.com/questions/14350675
     """
+    suffix = ".dcm"
     modality = None
     storage_type = None
     if attributes and "Modality" in attributes:
         modality = attributes.Modality
         storage_type = modality
 
-    ds = dicom.Dataset()
-    ds.file_meta = _default_meta(storage_type=storage_type)
+    # Create temporary file
+    temp_file = tempfile.NamedTemporaryFile(suffix=suffix).name
+    ds = dicom.dataset.FileDataset(temp_file, {},
+                                   file_meta=_default_meta(storage_type=storage_type),
+                                   preamble=b"\0" * 128)
+
 
     # Default settings. Can be overwritten later.
     ds.is_little_endian = True
@@ -109,13 +140,10 @@ def _ndarray2dicom(data: np.ndarray,
 
     ds.PhotometricInterpretation = "MONOCHROME2"
 
-
-    ds.BitsStored = data.itemsize*8
     ds.BitsAllocated = data.itemsize*8
+    ds.BitsStored = data.itemsize * 8
     ds.SamplesPerPixel = 1
     ds.HighBit = ds.BitsStored - 1
-
-    ds.ImagesInAcquisition = "1"
 
     ds.Rows = data.shape[0]
     ds.Columns = data.shape[1]
@@ -123,7 +151,10 @@ def _ndarray2dicom(data: np.ndarray,
 
     ds.PixelData = data.tobytes()
 
+    # ds.PixelRepresentation = 1 # signed byte (8-bit -> -128 ... 127)
+    ds.PixelRepresentation = 0 # unsigned byte (8-bit -> 0 ... 255)
 
+    # ds.ImagesInAcquisition = "1"
     # ds.ImagePositionPatient = r"0\0\1"
     # ds.ImageOrientationPatient = r"1\0\0\0\-1\0"
     # ds.ImageType = r"ORIGINAL\PRIMARY\AXIAL"
@@ -132,10 +163,9 @@ def _ndarray2dicom(data: np.ndarray,
     # ds.RescaleSlope = 1
     # ds.PixelSpacing = r"1\1"
 
-    ds.PixelRepresentation = 1 # needs to be adjusted depending on bit status (8bit = 0, 16bit = 1)
-
     if attributes:
         _apply_attributes(data=ds,
+                          meta=ds.file_meta,
                           attributes=attributes)
     dicom.dataset.validate_file_meta(ds.file_meta, enforce_standard=True)
     return ds
@@ -146,6 +176,7 @@ def stack2dicom(in_dir: PathLike,
                 pattern: Optional[str]=None,
                 regex: Optional[str]=None,
                 n_files: Optional[int]=None,
+                pix_depth: Optional[str]=None,
                 attributes: Optional[dicom.Dataset]=None,
                 show_progress: bool=True) -> None:
     in_dir = Path(in_dir)
@@ -162,7 +193,7 @@ def stack2dicom(in_dir: PathLike,
                                    enabled=show_progress)
     progress.start()
     for i, path in enumerate(paths):
-        img = _read_image(path=path)
+        img = _read_image(path=path, depth=pix_depth)
         if img is None:
             _logger.error("Skipping image %d...", i)
             continue
@@ -184,7 +215,6 @@ def dicom_2_nifti(in_dir: PathLike,
     if not ensure_out_dir(out_dir):
         return None
     try:
-        # header = common.read_dicom_directory(in_dir, out_dir)
         dicom2nifti.convert_directory(in_dir, out_dir, compression=comp, reorient=reor)
     except:
         _logger.error('Conversion to Nifti failed DICOM integrity compromised.\n'
@@ -194,10 +224,10 @@ def dicom_2_nifti(in_dir: PathLike,
 def nifti2dicom(in_dir: PathLike,
               out_dir: PathLike,
               pattern: Optional[str]=None,
-              regex: Optional[str] = None,
-              n_files: Optional[int] = None,
-              attributes: Optional[dicom.Dataset] = None,
-              show_progress: bool = True) -> None:
+              regex: Optional[str]=None,
+              n_files: Optional[int]=None,
+              attributes: Optional[dicom.Dataset]=None,
+              show_progress: bool=True) -> None:
 
     in_dir = Path(in_dir)
     out_dir = Path(out_dir)
@@ -228,6 +258,7 @@ def nifti2dicom(in_dir: PathLike,
         progress2.start()
         for s in range(number_slices):
             # array transpose to keep orientation
+            # check validity of array! PIL image
             ds = _ndarray2dicom(data=nii_array[:,:,s].T,
                                 attributes=attributes,
                                 instance_number=int(s+1)
